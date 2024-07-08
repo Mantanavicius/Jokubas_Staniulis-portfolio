e0232f6c20a47ee809284c1bcce9be2123d2a7af
Mantanavicius <mlantanavicius@gmail.com>
2024-07-08 19:47:23 +0300

    fixes to hero page

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+/////////////////////////////////////
+function animateGrain() {
+  const turbulence = document.querySelector("feTurbulence");
+  const fps = 20;
+  function animate() {
+    const frequency = 7 + Math.random(); // Adjust the range as needed
+    turbulence.setAttribute("baseFrequency", frequency);
+    setTimeout(() => {
+      requestAnimationFrame(animate);
+    }, 1000 / fps);
+  }
+  animate();
+}
+animateGrain();
+///////////////////////////////////////
 document.addEventListener("DOMContentLoaded", function () {
   let slides = document.querySelectorAll(".slide");
   let currentSlide = 0;
@@ -72,3 +87,59 @@ function resizeHeroText() {
 }
 resizeHeroText();
 window.addEventListener("resize", resizeHeroText);
+//////////////////////////////////////
+function menu() {
+  const parent = document.querySelector(".slide");
+  const child = document.querySelector("#hero-menu");
+
+  const parentWidth = parent.offsetWidth;
+  const parentHeight = parent.offsetHeight;
+
+  const imageAspectRatio = 1920 / 1080;
+  const parentAspectRatio = parentWidth / parentHeight;
+
+  let renderWidth,
+    renderHeight,
+    offsetX = 0,
+    offsetY = 0;
+  if (parentAspectRatio > imageAspectRatio) {
+    renderWidth = parentWidth;
+    renderHeight = renderWidth / imageAspectRatio;
+    offsetY = (parentHeight - renderHeight) / 2;
+  } else {
+    renderHeight = parentHeight;
+    renderWidth = renderHeight * imageAspectRatio;
+    offsetX = (parentWidth - renderWidth) / 2;
+  }
+
+  const squareWidth = (340 / 1920) * renderWidth;
+  const squareHeight = (24 / 1080) * renderHeight;
+  const squareLeft = (791 / 1920) * renderWidth + offsetX;
+  const squareTop = (646 / 1080) * renderHeight + offsetY;
+
+  child.style.width = squareWidth + "px";
+  child.style.height = squareHeight + "px";
+  child.style.left = squareLeft + "px";
+  child.style.top = squareTop + "px";
+}
+menu();
+window.addEventListener("resize", menu);
+///////////////////////////////////////
+function resizeMenuText() {
+  const parentElement = document.querySelector("#hero-menu");
+  const textElements = document.querySelectorAll(
+    "#hero .menu li a:not(.slash)"
+  );
+  const slashes = document.querySelectorAll(".slash");
+
+  const parentHeight = parentElement.offsetHeight;
+
+  for (let textElement of textElements) {
+    textElement.style.fontSize = parentHeight / 1.37 + "px";
+  }
+  for (let slash of slashes) {
+    slash.style.fontSize = parentHeight / 1.2 + "px";
+  }
+}
+resizeMenuText();
+window.addEventListener("resize", resizeMenuText);
